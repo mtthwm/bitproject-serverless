491707edb2a538936fcfe3383d71843849ce141e
Matthew <mtthwmrls9@gmail.com>
2022-04-27 17:30:50 -0600

    Added a find max function.

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,14 @@
 // start coding your function here!
+const find_max = (arr) => {
+    let currentMax = null;
+    arr.forEach(element => {
+        if (currentMax == null || currentMax < element)
+        {
+            currentMax = element
+        }
+    });
+
+    return currentMax;
+};
+
+exports.find_max = find_max;
--- tester.js
+++ tester.js
@@ -12,7 +12,14 @@ const main = () => {
     for (let i = 0; i < dates_to_try.length; i++)
     {
         const dateString = dates_to_try[i];
+        console.log(dateString, "->", running_late(dateString));
     }
+
+    console.log("=== TESTING max_nums.js... ===")
+    const find_max = require('./max_nums.js').find_max;
+    const testArray = [14, 64, 246, 56, 246.7];
+
+    console.log(testArray, find_max(testArray));
 };
 
 main();
